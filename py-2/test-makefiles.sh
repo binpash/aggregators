f33026951e6a72d08f9f9426e9115b96259feb70
Megan Zheng <mzheng37@cs.brown.edu>
2024-04-10 15:25:48 -0400

    change test generation

--- py-2/test-makefiles.sh
+++ py-2/test-makefiles.sh
@@ -13,8 +13,7 @@ SPLIT_SIZE=2
 split -dl $(($(wc -l < inputs/${FILE}.txt) / SPLIT_SIZE)) -a 1 --additional-suffix=${FILE_TYPE} ${INPUT_DIR}${FILE}.txt ${INPUT_DIR}/${FILE}-
 
 # Grab file + save as gb.txt
-wget --no-check-certificate 'https://atlas.cs.brown.edu/data/gutenberg/3/7/9/3790/3790.txt' -O inputs/gb.txt
-
 FILE="gb37"
 SPLIT_SIZE=5
+wget --no-check-certificate 'https://atlas.cs.brown.edu/data/gutenberg/3/7/9/3790/3790.txt' -O inputs/${FILE}${FILE_TYPE}
 split -dl $(($(wc -l < inputs/${FILE}.txt) / SPLIT_SIZE)) -a 1 --additional-suffix=${FILE_TYPE} ${INPUT_DIR}${FILE}.txt ${INPUT_DIR}/${FILE}-
