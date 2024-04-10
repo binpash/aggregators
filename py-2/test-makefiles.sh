8236d306b16bb380f561563793a822ef06de18da
Megan Zheng <mzheng37@cs.brown.edu>
2024-04-10 14:41:24 -0400

    splitted files

--- py-2/test-makefiles.sh
+++ py-2/test-makefiles.sh
@@ -5,8 +5,8 @@ INPUT_DIR="inputs/"
 FILE_TYPE=".txt"
 
 # Grab file + save as gb.txt
-wget -P inputs --no-check-certificate 'https://atlas.cs.brown.edu/data/gutenberg/0/1/old/1.txt' -O gb.txt
+wget --no-check-certificate 'https://atlas.cs.brown.edu/data/gutenberg/0/1/old/1.txt' -O inputs/gb.txt
 
 FILE="gb"
 SPLIT_SIZE=2
-split -l $(($(wc -l < inputs/yw.txt) / SPLIT_SIZE)) -a -d 1 --additional-suffix=${FILE_TYPE} ${INPUT_DIR}${FILE}.txt ${INPUT_DIR}/${FILE}- 
+split -dl $(($(wc -l < inputs/${FILE}.txt) / SPLIT_SIZE)) -a 1 --additional-suffix=${FILE_TYPE} ${INPUT_DIR}${FILE}.txt ${INPUT_DIR}/${FILE}- 
