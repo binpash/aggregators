561872252dd45581c5eb6139855bb840e1c74603
Megan Zheng <mzheng37@cs.brown.edu>
2024-04-16 14:00:41 -0400

    test

--- py-2/par-execute.sh
+++ py-2/par-execute.sh
@@ -1,1 +1,2 @@
 #!/bin/bash
+inputs-s/gb01-0.txt inputs-s/gb01-1.txt inputs-s/gb01-2.txt | xargs ./s_wc.py > outputs/gb01-par.txt
--- py-2/test-cmd-split.sh
+++ py-2/test-cmd-split.sh
@@ -14,27 +14,32 @@ INPUT_DIR="inputs/"
 OUTPUT_DIR="outputs/"
 FILE_TYPE=".txt"
 P='./par-execute.sh'
-CMDLIST=("wc")
+CMDLIST=(wc)
 
 # Build par execute file
 echo '#!/bin/bash' >$P
 chmod +x $P
 
-FILE="gb01"
+FULLFILE="gb01"
 SPLIT_SIZE=2
-wget --no-check-certificate 'https://atlas.cs.brown.edu/data/gutenberg/0/1/old/1.txt' -O inputs/${FILE}${FILE_TYPE}
+wget --no-check-certificate 'https://atlas.cs.brown.edu/data/gutenberg/0/1/old/1.txt' -O inputs/${FULLFILE}${FILE_TYPE}
 
 # SAME FOR ALL FILES
 mkdir inputs-s
-split -dl $(($(wc -l < inputs/${FILE}.txt) / SPLIT_SIZE)) -a 1 --additional-suffix=${FILE_TYPE} ${INPUT_DIR}${FILE}.txt inputs-s/${FILE}-
+split -dl $(($(wc -l < inputs/${FULLFILE}.txt) / SPLIT_SIZE)) -a 1 --additional-suffix=${FILE_TYPE} ${INPUT_DIR}${FULLFILE}.txt inputs-s/${FULLFILE}-
 # apply command to all split files
 for CMD in "${CMDLIST[@]}"; do
-    for FILE in "inputs-s/"*; do
-	mkfifo s.txt
-        "$CMD ${FILE}" > s.txt > "${FILE}"
+    cd inputs-s
+    for FILE in *; do
+	echo ${FILE} 
+	$CMD ${FILE} | sponge ${FILE} 
     done 
+    cd .. 
     chmod +x ./s_"$CMD".py
-    ./s_"$CMD".py int-* > ${OUTPUT_DIR}"${FILE}"-par.txt
+    filelist=$(ls -p inputs-s/*)
+    echo $filelist 
+    echo ""$filelist" | xargs ./s_$CMD.py > ${OUTPUT_DIR}${FULLFILE}-par.txt" >> $P
+    # echo "./s_"$CMD".py $filelist > ${OUTPUT_DIR}"${FULLFILE}"-par.txt" > $P
 done
 
 # Old piping method, doesn't seem to work but seems more efficient
