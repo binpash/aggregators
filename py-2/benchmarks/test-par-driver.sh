717e6f7739ffc85b406e6bc9816895d0ae2a9899
Megan Zheng <mzheng37@cs.brown.edu>
2024-07-22 15:00:05 -0400

    edit test-par-driver to be in general benchmarks folder

--- py-2/benchmarks/test-par-driver.sh
+++ py-2/benchmarks/test-par-driver.sh
@@ -14,7 +14,7 @@ shift
 CMD=$1
 shift
 
-AGG="../$1"
+AGG="../../$1"
 chmod +x $AGG
 shift
 
@@ -26,6 +26,9 @@ shift
 SPLIT_FILELIST="$1"
 shift
 
+CMD_SPLIT_FILE_DIR="$1"
+shift 
+
 IFS=' ' read -r -a split_filelist <<< $SPLIT_FILELIST
 
 FILENAME=$(basename "${FULLFILE}") # get filename (hi.txt)
@@ -34,18 +37,17 @@ WITHOUTTXT="${FILENAME%.*}"        # get filename without ext. (hi)
 CMD_FILE_NAME=$(echo "${CMD}" | awk '{print $1}')
 
 # make intermediate files for cmd applied to files
-mkdir -p inputs-s-par
-mkdir -p "inputs-s-par/${WITHOUTTXT}"
+mkdir -p ${CMD_SPLIT_FILE_DIR}
+mkdir -p "${CMD_SPLIT_FILE_DIR}/${WITHOUTTXT}"
 
 # for files in the temporary
 filelist=()
 for FILE in "${split_filelist[@]}"; do
     S_FILENAME=$(basename "${FILE}")                                                          # get filename (hi.txt)
     S_WITHOUTTXT="${S_FILENAME%.*}"                                                           # get filename without ext. (hi)
-    eval "cat "${FILE}" | "${CMD}" > inputs-s-par/"${WITHOUTTXT}/${CMD_FILE_NAME}-${S_WITHOUTTXT}.txt"" # apply command to all split file
-    filelist+="inputs-s-par/${WITHOUTTXT}/${CMD_FILE_NAME}-${S_WITHOUTTXT}.txt "
+    eval "cat "${FILE}" | "${CMD}" > ${CMD_SPLIT_FILE_DIR}/"${WITHOUTTXT}/${CMD_FILE_NAME}-${S_WITHOUTTXT}.txt"" # apply command to all split file
+    filelist+="${CMD_SPLIT_FILE_DIR}/${WITHOUTTXT}/${CMD_FILE_NAME}-${S_WITHOUTTXT}.txt "
 done
-
 echo "${AGG} ${filelist} > ${OUTPUT_DIR}${WITHOUTTXT}-${CMD_FILE_NAME}-par${FILE_TYPE}"
 echo "${AGG} ${filelist} > ${OUTPUT_DIR}${WITHOUTTXT}-${CMD_FILE_NAME}-par${FILE_TYPE}" >>"${P}" # print to accumulating  file
 # "./${AGG}" "${filelist}" > "${OUTPUT_DIR}${WITHOUTTXT}-${CMD_FILE_NAME}-par${FILE_TYPE}"         # run
