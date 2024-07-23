d9a7cad1157edf5f4c908c92ce96eca39d059977
Megan Zheng <mzheng37@cs.brown.edu>
2024-07-22 23:12:26 -0400

    add: covid-mts and missing agg

---
+++ py-2/benchmarks/covid-mts/cleanup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd "$(realpath $(dirname "$0"))"
+# rm -rf ./inputs
+rm -rf ./outputs
+rm -rf ./outputs/agg 
+rm -rf ./outputs-temp
+rm -rf ./inputs-s 
+rm -rf ./inputs-s-par
+rm -rf ./hashes
+rm execution.sh 
+rm log.txt
