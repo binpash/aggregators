ac2d4a70c4d1574806d66541a67611f02e6921ca
Megan <113795130+YUUU23@users.noreply.github.com>
2024-08-12 16:16:59 +0000

    ref: wc with reduce

--- py-2/s_wc.py
+++ py-2/s_wc.py
@@ -1,14 +1,33 @@
 #!/usr/bin/env python3
-import utils, re 
 
-def s_combine(parallel_res: list[str]):
+import utils, re, argparse, functools 
+
+parser = argparse.ArgumentParser(description="Check which flags we use for wc")
+parser.add_argument('-l', action='store_true', help="line count")
+parser.add_argument('-c', action='store_true', help="byte count")
+parser.add_argument('-w', action='store_true', help="word count")
+parser.add_argument('-m', action='store_true', help="char count")
+parser.add_argument('file', type=argparse.FileType('r'), nargs="*", help="input files to sort agg")
+args, unknown = parser.parse_known_args()
+
+def wc(parallel_res: list[str]):
     # maintain string pad length by splitting when number ends
     r = "\s*\d+" # for measuring padding size
     build_sum = parallel_res[0]
     for res in parallel_res[1:]:
         if res == None or res.strip() == "": continue 
         build_sum = "".join(str(int(x) + int(y)).rjust(len(x)) for x, y in zip(re.findall(r, build_sum), re.findall(r, res)))
-    return build_sum
+    return build_sum  
 
-read = utils.read_all_2()
-utils.write_file(s_combine(read)) 
+    
+def agg(a, b):
+    if len(a) == 0: 
+        parallel_res = [str(b)]
+    elif len(b) == 0: 
+        parallel_res = [str(a)]
+    else: 
+        parallel_res = [str(a),str(b)]
+    return wc(parallel_res)
+
+res = functools.reduce(agg, utils.read_all(), [])
+utils.out("".join(res)) 
