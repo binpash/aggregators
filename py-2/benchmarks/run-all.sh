b44a1a2ae4e74b22c7ea413f2c623d45badbe5b6
YUUU23 <megan_zheng@brown.edu>
2024-09-08 23:42:59 -0400

    add: run all script

--- py-2/benchmarks/run-all.sh
+++ py-2/benchmarks/run-all.sh
@@ -1,16 +1,44 @@
 #!/bin/bash
 
+RUN=0
 VERIFY=1
+CLEAN_UP=1
 
 # verify after running each benchmark / small or reg input
 if [[ "$@" == *"--v"* ]]; then
     VERIFY=0
-elif [[ "$@" == *"--small"* ]]; then
+fi
+
+# verify only
+if [[ "$@" == *"--v_only"* ]]; then
+    VERIFY=0
+    RUN=1
+fi
+
+# cleanup after running each benchmark / small or reg input
+if [[ "$@" == *"--c"* ]]; then
+    CLEAN_UP=0
+fi
+
+# clean up only
+if [[ "$@" == *"--c_only"* ]]; then
+    CLEAN_UP=0
+    RUN=1
+fi
+
+if [[ "$@" == *"--small"* ]]; then
     scripts_inputs=(
         "oneliners;small"
-        "unix50;reg"
-        "covid-mts;small"
-        "nlp;small"
+        # "unix50;reg"
+        # "covid-mts;small"
+        # "nlp;small"
+    )
+elif [[ "$@" == *"--single"* ]]; then
+    scripts_inputs=(
+        "oneliners;single"
+        # "unix50;reg"
+        # "covid-mts;small"
+        # "nlp;small"
     )
 else
     scripts_inputs=(
@@ -26,12 +54,16 @@ run() {
         IFS=";" read -r -a parsed <<<"${script_input}" # for every item in scripts_input; 0 = script and 1 = flag
         suite=${parsed[0]}
         flag=${parsed[1]}
-        echo "Running ${suite} benchmarks..."
-        cd "$(realpath "$(dirname "${suite}")")" || exit
-        if [[ $flag == "" ]]; then
-            ./run.sh
-        else
-            ./run.sh "${flag}"
+        cd "$(realpath "$(dirname "${suite}")")/${suite}" || exit
+
+        # if we should run scripts
+        if [[ $RUN == 0 ]]; then
+            echo "Running ${suite} benchmarks..."
+            if [[ $flag == "" ]]; then
+                ./run.sh
+            else
+                ./run.sh "--${flag}"
+            fi
         fi
 
         # if we should verify
@@ -39,7 +71,14 @@ run() {
             ./verify.sh --generate
         fi
 
+        # if we should clean-up
+        if [[ $CLEAN_UP == 0 ]]; then
+            ./cleanup.sh
+        fi
+
         cd ..
         echo "${suite} finished"
     done
 }
+
+run
