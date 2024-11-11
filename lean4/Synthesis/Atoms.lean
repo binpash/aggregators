c3ac7df3fac39e2095ffd58b5c22037f2d4f4d71
Chloe Qiao <qiaochloe@gmail.com>
2024-11-11 14:17:17 -0500

    Add concat aggregator

--- lean4/Synthesis/Atoms.lean
+++ lean4/Synthesis/Atoms.lean
@@ -1,3 +1,6 @@
+def concat : (x y : String) → String :=
+ String.append
+
 def merge (le : α → α → Bool) (xs ys : List α) : List α :=
   match xs, ys with
   | [], ys => ys
@@ -8,8 +11,10 @@ def merge (le : α → α → Bool) (xs ys : List α) : List α :=
     else
       y :: merge le (x :: xs) ys
 
+-- TODO: we might want to use `Prop` instead of `Bool` at some point
 -- def merge {α : Type} (r : α → α → Prop) [DecidableRel r] 
 --   : List α → List α → List α
 --   | [], l' => l'
 --   | l, [] => l
 --   | a :: l, b :: l' => if (r a b) then a :: merge r l (b :: l') else b :: merge r (a :: l) l'
+
