ac7a0663e8940c5f1a647b431836de6cc813c632
Chloe Qiao <qiaochloe@gmail.com>
2024-12-02 14:26:07 -0500

    Add aggregator for sort -u

--- lean4/Synthesis/Atoms.lean
+++ lean4/Synthesis/Atoms.lean
@@ -13,11 +13,3 @@ def merge (le : α → α → Bool) (xs ys : List α) : List α :=
       x :: merge le xs (y :: ys)
     else
       y :: merge le (x :: xs) ys
-
--- TODO: we might want to use `Prop` instead of `Bool` at some point
--- def merge {α : Type} (r : α → α → Prop) [DecidableRel r] 
---   : List α → List α → List α
---   | [], l' => l'
---   | l, [] => l
---   | a :: l, b :: l' => if (r a b) then a :: merge r l (b :: l') else b :: merge r (a :: l) l'
-
