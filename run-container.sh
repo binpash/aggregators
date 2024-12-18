cebaac6839550a45bc836dea10e799b01f9371e3
YUUU23 <megan_zheng@brown.edu>
2024-12-18 02:50:59 +0000

    docker organize

--- run-container.sh
+++ run-container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build -t agg .
-docker run -dit agg
+docker build -t agg -f docker/Dockerfile .
+docker run -it agg
 timestamp=$(date +"%Y-%m-%d %H:%M:%S")
 echo "[$timestamp] Container started."
