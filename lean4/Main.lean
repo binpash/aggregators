e2607edbfe1746fd61d446e30597fbb6ee8188f8
RobertoGonzalesMatos <roberto_gonzales_matos@brown.edu>
2024-12-23 00:16:10 -0500

    Feat: fixed synthesis

--- lean4/Main.lean
+++ lean4/Main.lean
@@ -1,15 +1,17 @@
-import Verification
+
+import Synthesis
+
+
+
+
 
 def main (args : List String) : IO UInt32 := do
-  -- let args : List System.FilePath := List.map (fun arg ↦ ⟨arg⟩) args
-  -- let streams ← getAllStreams args
-  --
-  -- let output ← List.foldlM (fun acc stream => do
-  --     let lines ← readFile stream []
-  --     let inputs := parseInput lines
-  --     let acc := merge (fun a b => a.key <= b.key) acc inputs
-  --     pure acc) 
-  --     [] streams
-  --
-  -- output.forM (fun output => IO.print output)
+  let streams ← getAllStreams args
+  let output ← List.foldlM 
+    (fun acc stream => do
+      let lines ← readFileByLine stream
+      let acc := uniq_agg acc lines.toListImpl
+      pure acc)
+    [] streams
+  output.forM (fun line => IO.print line)
   return 0
